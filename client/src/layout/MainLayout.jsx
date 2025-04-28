5c8280efedc5cbfb4b85a5f19509eb0b23728efb
Keshav Bansal <2022mcb1268@iitrpr.ac.in>
2025-04-28 13:09:53 +0530

    made bg fixed, updated text

--- client/src/layout/MainLayout.jsx
+++ client/src/layout/MainLayout.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const MainLayout = ({ children }) => {
     return (
-        <div className="flex flex-col min-h-screen bg-dark-glow bg-cover bg-center bg-no-repeat animate-pan-gradient">
+        <div className="flex flex-col min-h-screen bg-dark-glow bg-fixed bg-center bg-no-repeat animate-pan-gradient">
             <header className="bg-black bg-opacity-60 text-white p-4">
                 <h1 className="text-2xl font-extrabold text-center">DataTalk - Democratizing Data Accessibility</h1>
                 {/* Navigation or other header elements */}
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -23,7 +23,7 @@ const Home = () => {
                     href="/motto"
                     className="flex text-xl text-red-500 hover:text-red-600 hover:scale-105 font-extrabold justify-center transition-all duration-300 ease-in-out"
                 >
-                    Read the motto here
+                    Read my motto here
                 </a>
                 <QueryInput onSchemaInfo={handleSchemaInfo} />
 
